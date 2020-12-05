8858bc7555f8508ace4686822a795ea57bb96b82
MRAO0212 <mayureshvrao@gmail.com>
2020-12-04 23:22:16 -0800

    TIp Calculator

--- MVVMSample2/MVVMSample2/TableViewCell.swift
+++ MVVMSample2/MVVMSample2/TableViewCell.swift
@@ -12,7 +12,9 @@ class TableViewCell: UITableViewCell {
     var data:Model? {
         didSet {
             if let data = data {
-                
+                colorCircle.backgroundColor = data.color
+                colorView.backgroundColor = data.color.withAlphaComponent(0.5)
+                colorName.text = data.text
             }
         }
     }
@@ -23,8 +25,15 @@ class TableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        self.setupView()
     }
-
+    
+    private func setupView() {
+        colorView.layer.cornerRadius = 38
+        colorView.layer.masksToBounds = true
+//        colorCircle.layer.cornerRadius = 30
+//        colorCircle.layer.maskedCorners = [.layerMinXMinYCorner, .layerMinXMaxYCorner]
+    }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
--- MVVMSample2/MVVMSample2/ViewController.swift
+++ MVVMSample2/MVVMSample2/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet private weak var tableView: UITableView!
+    
+    var viewModel = ViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Mayuresh you got this man"
+        title = "MVVM"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TableViewCell
+        cell.data = viewModel.getModelAt(indexPath.row)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.count
+    }
+
+}
