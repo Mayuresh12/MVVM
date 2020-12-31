698167037be1d9e493ca8448cfa377384e2d7872
MRAO0212 <mayureshvrao@gmail.com>
2020-12-30 23:57:15 -0800

    add header and collection View

--- InstagramMVVM/InstagramMVVM2/Controller/MainTabController.swift
+++ InstagramMVVM/InstagramMVVM2/Controller/MainTabController.swift
@@ -49,10 +49,10 @@ class MainTabController: UITabBarController {
         let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController(collectionViewLayout: layout))
         let search = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"), rootViewController: SearchController())
         let image = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: ImageSelectorController())
-        let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileController(collectionViewLayout: layout))
         let notifications = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificationController())
+        let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileController(collectionViewLayout: layout))
         
-        viewControllers = [feed,search,image,profile,notifications]
+        viewControllers = [feed,search,image,notifications,profile]
         tabBar.tintColor = .black
     }
     
