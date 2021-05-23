419c4b145fff0911b1ff81a7c4f808611033ff92
MRAO0212 <mayureshvrao@gmail.com>
2021-05-23 00:52:03 -0700

    complete authentication screen

--- InstagramMVVM/InstagramMVVM3/InstagramMVVM3/Utils/Extensions.swift
+++ InstagramMVVM/InstagramMVVM3/InstagramMVVM3/Utils/Extensions.swift
@@ -9,6 +9,17 @@
 import UIKit
 import JGProgressHUD
 
+extension UIButton {
+    func attributedTitle2(firstPart: String, secondPart: String) {
+        let atts: [NSAttributedString.Key : Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7),
+                                                    .font: UIFont.systemFont(ofSize: 16)]
+        let attributedTitle = NSMutableAttributedString(string: "\(firstPart) ", attributes: atts)
+        let boldAtts: [NSAttributedString.Key : Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7),
+                                                    .font: UIFont.boldSystemFont(ofSize: 16)]
+        attributedTitle.append(NSAttributedString(string: secondPart, attributes: boldAtts))
+        setAttributedTitle(attributedTitle, for: .normal)
+    }
+}
 extension UIViewController {
     static let hud = JGProgressHUD(style: .dark)
     
