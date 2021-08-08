db39dc914e8760f570d8a79a7038d1d4f479d30c
MRAO0212 <mayureshvrao@gmail.com>
2021-08-07 21:14:53 -0700

    fetching notfications

--- InstagramMVVM3/InstagramMVVM3/Controller/NotificationController.swift
+++ InstagramMVVM3/InstagramMVVM3/Controller/NotificationController.swift
@@ -13,11 +13,24 @@ class NotificationsController: UITableViewController {
     
     // MARK: Properties
     
+    private var notifications = [Notification]() {
+        didSet { tableView.reloadData() }
+    }
+    
     // MARK: Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        fetchNotifications()
+    }
+    
+    //MARK: Api
+    
+    func fetchNotifications(){
+        NotificationService.fetchNotifications { notifications in
+            self.notifications = notifications
+        }
     }
     
     // MARK: Helpers
@@ -34,7 +47,7 @@ class NotificationsController: UITableViewController {
 
 extension NotificationsController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return notifications.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
